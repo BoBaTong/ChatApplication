ae769f9e3e9de5280410fb59e8dac57ba1b00cb0
tuvaneiu <59410808+tuvaneiu@users.noreply.github.com>
2021-01-30 11:23:50 +0700

    junit

--- src/com/student/test/DataStorageUnitTest.java
+++ src/com/student/test/DataStorageUnitTest.java
@@ -3,18 +3,24 @@ package com.student.test;
 import com.student.DataStorage;
 import com.student.group.Group;
 import com.student.repository.UserRepository;
+import com.student.services.UserServices;
 import com.student.user.User;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-public class DataStorageUnitTest {
+class DataStorageUnitTest {
+    UserRepository userRepository;
 
+    @BeforeEach
+    void setUp() throws Exception{
+        userRepository = new UserRepository();
+    }
 
     @Test
-    public void getUserTest() {
-        UserRepository userRepository = new UserRepository();
+    void getUserTest() {
         User user = new User("Tong", "Bo", "Bo Ba Tong", "tongbo", "123456", "Male", "11/04/1997");
         userRepository.save(user);
 //        User result = userServices.findById(user.getId(), User.class);
@@ -22,14 +28,12 @@ public class DataStorageUnitTest {
         Assertions.assertEquals(0, DataStorage.getInstance().getUserCollection().size());
     }
     @Test
-    public void findAllTest()
+    void findAllTest()
     {
-        UserRepository userRepository = new UserRepository();
         User user = new User("Tong", "Bo", "Bo Ba Tong", "tongbo", "123456", "Male", "11/04/1997");
         User user2 = new User("Tong2", "Bo2", "Bo Ba Tong2", "tongbo2", "123456", "Male", "11/04/1997");
         userRepository.save(user);
         userRepository.save(user2);
-        List list = userRepository.findAll(Group.class);
         Assertions.assertEquals(2, DataStorage.getInstance().getUserCollection().size());
     }
 }
