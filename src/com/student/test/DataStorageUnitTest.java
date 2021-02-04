9617713cf17053ba235a8d65d00a8ac8e3548cd9
binheiu1997 <59431424+binheiu1997@users.noreply.github.com>
2021-02-04 22:52:37 +0700

    nothing change
    look up for code

--- src/com/student/test/DataStorageUnitTest.java
+++ src/com/student/test/DataStorageUnitTest.java
@@ -23,24 +23,28 @@ class DataStorageUnitTest {
     public void findByFieldTest() throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException {
         UserRepository userRepository = new UserRepository();
         User user = new User("Tong", "Bo", "Bo Ba Tong", "tongbo", "123456", "Male", "11/04/1997");
-        User user2 = new User("Tong", "Bo", "Bo Ba Tong", "tongbo2", "123456", "Male", "11/04/1997");
+        User user2 = new User("Tong2", "Bo", "Bo Ba Tong2", "tongbo2", "123456", "Male", "11/04/1997");
 
         userRepository.save(user);
         userRepository.save(user2);
 
         User user3 = DataStorage.getInstance().findByField("Bo","firstName",User.class);
+        //Assertions.assertEquals();
         System.out.println(user3);
     }
     @Test
     void getUserTest() {
         User user = new User("Tong", "Bo", "Bo Ba Tong", "tongbo", "123456", "Male", "11/04/1997");
+
         userRepository.save(user);
 //        User result = userServices.findById(user.getId(), User.class);
         userRepository.removeById(user.getId(),User.class);
         Assertions.assertEquals(0, DataStorage.getInstance().getUserCollection().size());
     }
-    @Test
-    void findAllTest()
+
+
+    @Test                 
+    public void findAllTest()
     {
         User user = new User("Tong", "Bo", "Bo Ba Tong", "tongbo", "123456", "Male", "11/04/1997");
         User user2 = new User("Tong2", "Bo2", "Bo Ba Tong2", "tongbo2", "123456", "Male", "11/04/1997");
