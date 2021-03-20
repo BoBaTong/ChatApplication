0b90f20b87ed75b8e4e5649aa0605146c9cbaa80
BoBaTong <tong.bo.set16@eiu.edu.vn>
2021-03-20 10:03:27 +0700

    final part 2
    final added some changes

--- src/com/student/Main.java
+++ src/com/student/Main.java
@@ -1,43 +1,47 @@
 package com.student;
 
-
 import com.student.message.ContentType;
 import com.student.message.Message;
 import com.student.services.GroupServices;
 import com.student.services.MessageServices;
 import com.student.services.UserServices;
-import com.student.user.User;
 
 public class Main {
 
-    public static void main(String[] args) throws NoSuchFieldException,IllegalAccessException {
-
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
         GroupServices groupServices = new GroupServices();
         UserServices userServices = new UserServices();
         DataStorage dataStorage = DataStorage.getInstance();
         MessageServices messageServices = new MessageServices();
-//        User user = new User("tongbo","tong","fsf","tongbo","fkwqopfk","male","312313");
-        userServices.saveUser("tongbo","tong","fsf","tongbo","fkwqopfk","male","312313");
+        userServices.saveUser("tongbo","tong","fsf","tongbo","fkwqopfk");
+
+//        userServices.saveUser("tongbo","tong","fsf","tongbo","fkwqopfk","male","312313");
+//        userServices.saveUser("tongbo2","tong2","fsf","tongbo2","fkwqopfk","male","312313");
+
         groupServices.createGroup(1,0);
-
-//        groupServices.createGroup(1,user);
-        User user2 = new User("tongbo2","tong","fsf","tongbo","fkwqopfk","male","312313");
-//        groupServices.addMemberPublicGroupByInviteCode("41c36ae4-b035-45d1-89dc-e6f235d45954",user2);
-
-        System.out.println("id"+dataStorage.getPublicGroupCollection().get(0).getId());
-//        System.out.println(dataStorage.getPublicGroupCollection().get(1).getId());
-//        groupServices.addMemberPublicGroupByMember(user,user2,1);
-//        System.out.println(dataStorage.getPublicGroupCollection().get(1).getListOfUser().size());
-        System.out.println(dataStorage.getPublicGroupCollection().get(0).getListOfUser().size());
-        System.out.println("show"+dataStorage.getUserCollection().size());
-//        groupServices.addMemberPrivateGroupByAdmin(user,user2,0);
-
-//        System.out.println(dataStorage.getPrivateGroupCollection().get(0).getListOfUser());
-//        System.out.println(dataStorage.getPrivateGroupCollection().get(0).getAdminGroup());
+        groupServices.createGroup(0,0);
         Message message = new Message("ccccccccc", ContentType.Text);
-        messageServices.sendMessage(message,0,0);
-        messageServices.showMessagePublicGroup(0,dataStorage.getPublicGroupCollection().get(0));
+        Message message2 = new Message("ccccccccc2222", ContentType.Text);
+        Message message3 = new Message("user1 sent to user2", ContentType.Text);
 
+//        System.out.println("show2"+dataStorage.getGroupCollection().size());
+//        messageServices.sendMessageUserToGroup(message,0,0);
+//        messageServices.sendMessageUserToGroup(message2,0,1);
+//        messageServices.showMessageGroup(0,0);
+//        messageServices.showMessageGroup(0,1);
+//        messageServices.sendMessageUserToUser(message3,0,1);
+//        messageServices.showMessageUser(0,1);
+//        System.out.println(dataStorage.getGroupCollection().get(0).getInviteCode());
+//        System.out.println(dataStorage.getGroupCollection().get(1).getInviteCode());
+//        System.out.println(dataStorage.getGroupCollection().get(0).getListOfUser().size());
+//
+//        groupServices.addMemberByInviteCode(dataStorage.getGroupCollection().get(1).getInviteCode(),dataStorage.getUserCollection().get(1));
+//        System.out.println(dataStorage.getGroupCollection().get(0).getListOfUser().size());
+        System.out.println("3 "+dataStorage.getGroupCollection().size());
+        System.out.println("2 "+dataStorage.getUserCollection().size());
+        System.out.println("1 "+dataStorage.getGroupCollection().get(0).getListOfUser());
 
+        groupServices.leaveGroup(dataStorage.getUserCollection().get(0),dataStorage.getGroupCollection().get(0));
+        System.out.println(dataStorage.getGroupCollection().get(0).getListOfUser());
     }
 }
